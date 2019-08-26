b203c60100468d8e68b7da58d8097d5301032732
Chuck Lu <chuck.lu@qq.com>
2019-08-26 12:56:59 +0800

    await custom awaitable class instance

--- async-await-test/MainTest.cs
+++ async-await-test/MainTest.cs
@@ -10,8 +10,16 @@ namespace async_await_test
     [TestFixture]
     public class MainTest
     {
+        [Test]
         public void Test1()
         {
+
+        }
+
+        static async Task AwaitMyAwaitable()
+        {
+            MyAwaitableClass class1 = new MyAwaitableClass();
+            await class1;
 
         }
     }
