2edec26fef20195fe66dd772c35eea5cf0f79201
Chuck Lu <chuck.lu@qq.com>
2019-08-26 12:52:52 +0800

    add a new project for test

---
+++ async-await-test/MainTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace async_await_test
+{
+    [TestFixture]
+    public class MainTest
+    {
+        public void Test1()
+        {
+
+        }
+    }
+}
