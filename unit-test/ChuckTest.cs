cbf573e063fa76e954deb1234829e5a10ece7a56
Chuck LU <chucklu2015@outlook.com>
2021-09-18 17:13:30 +0800

    another test case

--- unit-test/ChuckTest.cs
+++ unit-test/ChuckTest.cs
@@ -14,8 +14,8 @@ namespace unit_test
         [Test]
         public async Task Test20210918_001()
         {
-            Console.WriteLine($"in time is {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}"); 
-            await GetNumber();
+            Console.WriteLine($"in time is {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+            await GetNumber().ConfigureAwait(false);
             Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}===");
             Console.WriteLine($"out time is {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
         }
@@ -23,6 +23,34 @@ namespace unit_test
         public async Task GetNumber()
         {
             await Task.Delay(5000);
+        }
+
+        [Test]
+        public async Task Test20210918_002()
+        {
+            await MyMethodAsync();
+        }
+
+        public async Task MyMethodAsync()
+        {
+            Console.WriteLine($"chuck1 time is {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+            Task<int> longRunningTask = LongRunningOperationAsync();
+            // independent work which doesn't need the result of LongRunningOperationAsync can be done here
+
+            //and now we call await on the task 
+            Console.WriteLine($"chuck2 time is {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+            //int result = await longRunningTask;
+            await longRunningTask;
+            Console.WriteLine($"chuck3 time is {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+            //use the result 
+            //Console.WriteLine(result);
+            Console.WriteLine($"chuck4 time is {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+        }
+
+        public async Task<int> LongRunningOperationAsync() // assume we return an int from this long running operation 
+        {
+            await Task.Delay(3000); // 3 seconds delay
+            return 1;
         }
     }
 }
