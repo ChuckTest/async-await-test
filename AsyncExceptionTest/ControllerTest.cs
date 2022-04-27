a522fc9a766178517b611f9cb0c523a05ffa84fd
Chuck LU <chucklu2015@outlook.com>
2022-04-27 10:42:06 +0800

    test for https://stackoverflow.com/questions/15410661/is-it-possible-to-get-a-good-stack-trace-with-net-async-methods

---
+++ AsyncExceptionTest/ControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncExceptionTest
+{
+    class ControllerTest
+    {
+        public double GetValueAction()
+        {
+            return this.GetValue().Result;
+        }
+
+        public async Task<double> GetValue()
+        {
+            return await this.GetValue2().ConfigureAwait(false);
+        }
+
+        public async Task<double> GetValue2()
+        {
+            throw new InvalidOperationException("Couldn't get value!");
+        }
+    }
+}
---
+++ AsyncExceptionTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ControllerTest controllerTest = new ControllerTest();
+                controllerTest.GetValueAction();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
