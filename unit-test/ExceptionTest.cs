885223b793b5adeb0c7ef68ce5d21e5093f729a8
Chuck LU <chucklu2015@outlook.com>
2022-07-03 22:37:18 +0800

    add exception test

---
+++ unit-test/ExceptionTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace unit_test
+{
+    //https://stackoverflow.com/questions/12144077/async-await-when-to-return-a-task-vs-void
+    [TestFixture]
+    class ExceptionTest
+    {
+        static async void f()
+        {
+            await h();
+        }
+
+        static async Task g()
+        {
+            await h();
+        }
+
+        static async Task h()
+        {
+            //await Task.Delay(1000);
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void button1_Click()
+        {
+            try
+            {
+                f();
+            }
+            catch (Exception ex)
+            {
+                CustomConsole.WriteLine(ex.ToString());
+            }
+        }
+
+        [Test]
+        public void button2_Click()
+        {
+            try
+            {
+                g();
+            }
+            catch (Exception ex)
+            {
+                CustomConsole.WriteLine(ex.ToString());
+            }
+        }
+
+        [Test]
+        public void button3_Click()
+        {
+            try
+            {
+                GC.Collect();
+            }
+            catch (Exception ex)
+            {
+                CustomConsole.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
