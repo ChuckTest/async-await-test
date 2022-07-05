297fc0453c0b7d359e68b4314f664c20b718bdcd
Chuck LU <chucklu2015@outlook.com>
2022-07-05 16:37:02 +0800

    add test case from https://stackoverflow.com/questions/19813719/await-keyword-blocks-main-thread

---
+++ unit-test/UnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace unit_test
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        private async void button1_Click()
+        {
+            await DoSomethingAsync();
+        }
+
+        private async Task DoSomethingAsync()
+        {
+            await Task.Delay(3000); // simulate job
+
+            CustomConsole.WriteLine("DoSomethingAsync is done");
+
+            await DoSomething2Async();
+        }
+
+        private async Task DoSomething2Async()
+        {
+            await Task.Delay(3000); // simulate job
+
+            CustomConsole.WriteLine("DoSomething2Async is done");
+        }
+
+        [Test]
+        public void Test2022_0705_001()
+        {
+            button1_Click();
+            CustomConsole.WriteLine("Hello world");
+        }
+    }
+}
