c37bc6ff7fd791d726d659a7d97696f04dc79a82
Chuck LU <chucklu2015@outlook.com>
2022-07-05 16:47:06 +0800

    test case for await

--- AsyncExceptionTest/Program.cs
+++ AsyncExceptionTest/Program.cs
@@ -9,12 +9,35 @@ namespace AsyncExceptionTest
 {
     class Program
     {
+        private static async void button1_Click()
+        {
+            Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fffzzz} enter button1_Click");
+            await DoSomethingAsync();
+        }
+
+        private static async Task DoSomethingAsync()
+        {
+            await Task.Delay(3000); // simulate job
+
+            Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fffzzz} DoSomethingAsync is done");
+
+            await DoSomething2Async();
+        }
+
+        private static async Task DoSomething2Async()
+        {
+            await Task.Delay(3000); // simulate job
+
+            Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fffzzz} DoSomething2Async is done");
+        }
+
         static void Main(string[] args)
         {
             try
             {
-                ControllerTest controllerTest = new ControllerTest();
-                controllerTest.GetValueAction();
+                //ControllerTest controllerTest = new ControllerTest();
+                //controllerTest.GetValueAction();
+                button1_Click();
             }
             catch (Exception ex)
             {
